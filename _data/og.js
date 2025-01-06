a000a16202961ccf15a716fa4b3a2ed16f064116
Will <145696026+will-bbg@users.noreply.github.com>
2025-01-05 23:33:36 -0600

    feat: adds liquid templates for footer and scripts; data for og info

---
+++ _data/og.js
@@ -0,0 +1,9 @@
+module.exports = {
+    default: {
+      title: "SimLEARN",
+      url: "https://innovation.va.gov/simlearn",
+      description: "Serving The Largest Integrated Health Care System.",
+      type: "Website",
+      image: "https://innovation.bgsandbox.com/simlearn/assets/img/og-image.jpg",
+    },
+  };
---
+++ _data/site.js
@@ -0,0 +1,3 @@
+module.exports = {
+    baseUrl: "https://innovation.va.gov/simlearn",
+  };
