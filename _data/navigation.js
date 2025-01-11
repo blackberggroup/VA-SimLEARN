d18f1aecc128d42d38ea6415e0dd2de2c093bcf8
Will <145696026+will-bbg@users.noreply.github.com>
2025-01-11 17:20:44 -0600

    feat: moved nav links into a .js file; updated header to build from navigaiton.js

---
+++ _data/navigation.js
@@ -0,0 +1,62 @@
+module.exports = {
+  primaryLinks: [
+    { 
+      label: "Home", 
+      url: "/", 
+      submenu: null 
+    },
+    {
+      label: "About Us",
+      submenu: [
+        { label: "About SimLEARN", url: "/about-us/" },
+        { label: "SimLEARN Leadership Team", url: "/about-us/simlearn-leadership/" },
+        { label: "Contact Us", url: "/about-us/contact-us/" }
+      ]
+    },
+    { 
+      label: "News & Media", 
+      url: "/news-and-media/", 
+      submenu: null 
+    },
+    {
+      label: "Portfolios",
+      megaMenu: [
+        {
+          title: "SimLEARN",
+          description: "Simulation Validation, Evaluation, and Testing",
+          url: "/portfolio/simlearn/"
+        },
+        {
+          title: "REdI",
+          description: "Resuscitation Education and Innovation",
+          url: "/portfolio/redi/"
+        },
+        {
+          title: "LM",
+          description: "Learning Management",
+          url: "/portfolio/lm/"
+        },
+        {
+          title: "ACO",
+          description: "Assessment, Collaboration and Outreach",
+          url: "/portfolio/aco/"
+        },
+        {
+          title: "CTE",
+          description: "Clinical Training and Engagement",
+          url: "/portfolio/cte/"
+        },
+        {
+          title: "SO",
+          description: "Support Operations",
+          url: "/portfolio/so/"
+        }
+      ]
+    },
+    { 
+      label: "Course Catalog", 
+      url: "/course-catalog/", 
+      submenu: null 
+    }
+  ]
+};
