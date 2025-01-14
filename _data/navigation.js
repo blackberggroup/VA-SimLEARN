87a1643480b6361d107f2d0802f73f88bf704939
Will <145696026+will-bbg@users.noreply.github.com>
2025-01-14 12:46:10 -0600

    bug: nav data update; assetPath update in header

--- _data/navigation.js
+++ _data/navigation.js
@@ -8,13 +8,13 @@ module.exports = {
     {
       label: "About Us",
       submenu: [
-        { label: "About SimLEARN", url: "about-us/" },
-        { label: "SimLEARN Leadership Team", url: "about-us/simlearn-leadership/" },
+        { label: "About SimLEARN", url: "/about-us/" },
+        { label: "SimLEARN Leadership Team", url: "/about-us/simlearn-leadership/" },
       ]
     },
     { 
       label: "News & Media", 
-      url: "news-and-media/", 
+      url: "/news-and-media/", 
       submenu: null 
     },
     {
@@ -23,43 +23,43 @@ module.exports = {
         {
           title: "SimLEARN",
           description: "Simulation Validation, Evaluation, and Testing",
-          url: "portfolio/simlearn/"
+          url: "/portfolio/simlearn/"
         },
         {
           title: "REdI",
           description: "Resuscitation Education and Innovation",
-          url: "portfolio/redi/"
+          url: "/portfolio/redi/"
         },
         {
           title: "LM",
           description: "Learning Management",
-          url: "portfolio/lm/"
+          url: "/portfolio/lm/"
         },
         {
           title: "ACO",
           description: "Assessment, Collaboration and Outreach",
-          url: "portfolio/aco/"
+          url: "/portfolio/aco/"
         },
         {
           title: "CTE",
           description: "Clinical Training and Engagement",
-          url: "portfolio/cte/"
+          url: "/portfolio/cte/"
         },
         {
           title: "SO",
           description: "Support Operations",
-          url: "portfolio/so/"
+          url: "/portfolio/so/"
         }
       ]
     },
     { 
       label: "Course Catalog", 
-      url: "course-catalog/", 
+      url: "/course-catalog/", 
       submenu: null 
     },
     { 
       label: "Contact Us", 
-      url: "contact-us/", 
+      url: "/contact-us/", 
       submenu: null 
     },    
   ]
